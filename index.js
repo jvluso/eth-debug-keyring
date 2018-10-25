837e276100c4565f62b8652ab6a79e4fe71a9c32
jeremy <jvluso@yahoo.com>
2018-10-24 17:05:16 -0700

    Aragon keyring

--- index.js
+++ index.js
@@ -20,67 +20,51 @@ class HdKeyring extends EventEmitter {
 
   serialize () {
     return Promise.resolve({
-      mnemonic: this.mnemonic,
-      numberOfAccounts: this.wallets.length,
-      hdPath: this.hdPath,
+      ens: this.ens,
+      dao: this.dao,
+      forwardingAddress: this.forwardingAddress,
+      parentAddress: this.parentAddress
     })
   }
 
   deserialize (opts = {}) {
     this.opts = opts || {}
-    this.wallets = []
-    this.mnemonic = null
-    this.root = null
-    this.hdPath = opts.hdPath || hdPathString
-
-    if (opts.mnemonic) {
-      this._initFromMnemonic(opts.mnemonic)
-    }
-
-    if (opts.numberOfAccounts) {
-      return this.addAccounts(opts.numberOfAccounts)
-    }
+    this.wrapper = {}
+    this.ens = ens
+    this.dao = dao
+    this.forwardingAddress = sigUtil.normalize(forwardingAddress)
+    this.parentAddress = sigUtil.normalize(parentAddress)
 
     return Promise.resolve([])
   }
 
   addAccounts (numberOfAccounts = 1) {
-    if (!this.root) {
-      this._initFromMnemonic(bip39.generateMnemonic())
-    }
-
-    const oldLen = this.wallets.length
-    const newWallets = []
-    for (let i = oldLen; i < numberOfAccounts + oldLen; i++) {
-      const child = this.root.deriveChild(i)
-      const wallet = child.getWallet()
-      newWallets.push(wallet)
-      this.wallets.push(wallet)
-    }
-    const hexWallets = newWallets.map((w) => {
-      return sigUtil.normalize(w.getAddress().toString('hex'))
-    })
-    return Promise.resolve(hexWallets)
+    return Promise.resolve([forwardingAddress])
   }
 
   getAccounts () {
-    return Promise.resolve(this.wallets.map((w) => {
-      return sigUtil.normalize(w.getAddress().toString('hex'))
-    }))
+    return Promise.resolve([forwardingAddress])
   }
 
   // tx is an instance of the ethereumjs-transaction class.
   signTransaction (address, tx) {
-    const wallet = this._getWalletForAccount(address)
-    var privKey = wallet.getPrivateKey()
-    tx.sign(privKey)
-    return Promise.resolve(tx)
+    return this._getWallet()
+    .then((wrapper) =>{
+      return wrapper.calculateForwardingPath(
+        this.parentAddress,
+        tx.to,
+        tx,
+        [tx.from])
+    })
+    .then(result => {
+      this.subProvider.send(result[0],this.parentAddress)
+    })
   }
 
   // For eth_sign, we need to sign transactions:
   // hd
   signMessage (withAccount, data) {
-    const wallet = this._getWalletForAccount(withAccount)
+    const wallet = this._getWallet()
     const message = ethUtil.stripHexPrefix(data)
     var privKey = wallet.getPrivateKey()
     var msgSig = ethUtil.ecsign(new Buffer(message, 'hex'), privKey)
@@ -90,7 +74,7 @@ class HdKeyring extends EventEmitter {
 
   // For personal_sign, we need to prefix the message:
   signPersonalMessage (withAccount, msgHex) {
-    const wallet = this._getWalletForAccount(withAccount)
+    const wallet = this._getWallet()
     const privKey = ethUtil.stripHexPrefix(wallet.getPrivateKey())
     const privKeyBuffer = new Buffer(privKey, 'hex')
     const sig = sigUtil.personalSign(privKeyBuffer, { data: msgHex })
@@ -99,7 +83,7 @@ class HdKeyring extends EventEmitter {
 
   // personal_signTypedData, signs data along with the schema
   signTypedData (withAccount, typedData) {
-    const wallet = this._getWalletForAccount(withAccount)
+    const wallet = this._getWallet()
     const privKey = ethUtil.toBuffer(wallet.getPrivateKey())
     const signature = sigUtil.signTypedData(privKey, { data: typedData })
     return Promise.resolve(signature)
@@ -107,7 +91,7 @@ class HdKeyring extends EventEmitter {
 
   // For eth_sign, we need to sign transactions:
   newGethSignMessage (withAccount, msgHex) {
-    const wallet = this._getWalletForAccount(withAccount)
+    const wallet = this._getWallet()
     const privKey = wallet.getPrivateKey()
     const msgBuffer = ethUtil.toBuffer(msgHex)
     const msgHash = ethUtil.hashPersonalMessage(msgBuffer)
@@ -117,27 +101,25 @@ class HdKeyring extends EventEmitter {
   }
 
   exportAccount (address) {
-    const wallet = this._getWalletForAccount(address)
+    const wallet = this._getWallet()
     return Promise.resolve(wallet.getPrivateKey().toString('hex'))
   }
 
 
   /* PRIVATE METHODS */
 
-  _initFromMnemonic (mnemonic) {
-    this.mnemonic = mnemonic
-    const seed = bip39.mnemonicToSeed(mnemonic)
-    this.hdWallet = hdkey.fromMasterSeed(seed)
-    this.root = this.hdWallet.derivePath(this.hdPath)
-  }
 
-
-  _getWalletForAccount (account) {
-    const targetAddress = sigUtil.normalize(account)
-    return this.wallets.find((w) => {
-      const address = sigUtil.normalize(w.getAddress().toString('hex'))
-      return ((address === targetAddress) ||
-              (sigUtil.normalize(address) === targetAddress))
+  _getWallet () {
+    if(this.wrapper){
+      return (promise.resolve(this.wrapper))
+    }
+    return initAragonJS(this.dao, this.ens, {
+      accounts: this.accounts,
+      provider: this.subProvider
+    })
+    .then((initializedWrapper) => {
+      this.wrapper = initializedWrapper
+      return this.wrapper
     })
   }
 }
